2d469b2a7a8aa69fae72ce161904465ed06cc343
Stella Mazzillo <stellagress5@gmail.com>
2023-03-13 16:57:27 -0400

    complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+
+const currentUser = "Stella";
+
+//const welcomeMessage = "Welcome to Flatbook, ";
+
+
+const welcomeMessage = (`Welcome to Flatbook, ${currentUser}!`); 
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+//const shortGreeting = "Welcome, S!" ;
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
